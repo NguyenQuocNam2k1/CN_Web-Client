adde0b3e4808a401bcb6f5bf44a78a711dfb5dcd
NQNam <namtv27072001@gmail.com>
2022-01-10 22:34:47 +0700

    created redux

---
+++ src/redux/reducers/userReducers.js
@@ -0,0 +1,14 @@
+import { UserTypes } from "../contants/action-types"
+
+
+const initialState = {}
+
+export const userReducer = (state = initialState , {type , payload}) =>{
+    switch (type) {
+        case UserTypes.UserTypes:
+            
+            break;
+        default:
+            break;
+    }
+}
