25104264fbd5f771038acc039eb0a71c711564be
NQNam <namtv27072001@gmail.com>
2022-01-13 16:32:08 +0700

    pull code

--- src/component/pages/Home/Home.js
+++ src/component/pages/Home/Home.js
@@ -4,18 +4,44 @@ import { Link } from "react-router-dom";
 function Home(props) {
   return (
     <div className='container'>
-      <div className='row hompage_layout_1'>
+      <div className='row homepage_layout_1'>
         <div className='col-5'></div>
         <div className='col-1'></div>
         <div className='col-6'>
           <h1>Tham gia học MIỄN PHÍ</h1>
-          <Link to="/user/register">
+          <Link to='/user/register'>
             <button className='button_homepage'>Đăng ký</button>
           </Link>
           <p>Hoặc đăng ký bằng</p>
-          {for(let index ; index <= 3 ; index++){
-              
-          }}
+        </div>
+      </div>
+      <div className='homepage_layout_2'>
+        <h1>Lộ trình</h1>
+        <div className='row justify-content-around'>
+          <div className='col-4'>
+            <div className='homepage_layout_2_image'>
+              <Link to='/user/register'>
+                <button>Đăng ký</button>
+              </Link>
+            </div>
+            <p className='homepage_layout_2_text'>Mới bắt đầu</p>
+          </div>
+          <div className='col-4'>
+            <div className='homepage_layout_2_image'>
+              <Link to='/user/register'>
+                <button>Đăng ký</button>
+              </Link>
+            </div>
+            <p className='homepage_layout_2_text'>Font-End</p>
+          </div>
+          <div className='col-4'>
+            <div className='homepage_layout_2_image'>
+              <Link to='/user/register'>
+                <button>Đăng ký</button>
+              </Link>
+            </div>
+            <p className='homepage_layout_2_text'>Back-End</p>
+          </div>
         </div>
       </div>
     </div>
