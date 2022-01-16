068911e2c2cf86fc9241aba2681bab93d4dcdbf5
Bui Thinh <buithinh2507@gmail.com>
2022-01-16 10:54:11 +0700

    get  cookie

--- src/redux/reducers/userReducers.js
+++ src/redux/reducers/userReducers.js
@@ -13,7 +13,7 @@ export const userReducer = (state = initialState, { type, payload }) => {
 
                 let valueToken = payload.token;
                 // Set a Cookie
-                function setCookie( nameCookie, valueCookie, numberDays) {
+                function setCookie(nameCookie, valueCookie, numberDays) {
                     let date = new Date();
                     date.setTime(date.getTime() + (numberDays * 24 * 60 * 60 * 1000));
                     const expires = "expires=" + date.toUTCString();
