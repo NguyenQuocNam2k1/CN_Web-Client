f4aa59bac0fa14a18acf00b060e3a2ee7f99d15c
NQNam <namtv27072001@gmail.com>
2022-02-21 14:14:50 +0700

    _

--- src/component/pages/header/Header.js
+++ src/component/pages/header/Header.js
@@ -73,12 +73,11 @@ function Header() {
                   aria-label="Search"
                 />
                 <button className="btn" type="submit">
-                  {" "}
                   <img
                     src={search}
                     className="img-input"
                     alt="img-input"
-                  />{" "}
+                  />
                 </button>
               </form>
               <Link to="/user">
