96116904247ae5e7f6e56672e16ef29a52e024e1
Ngoc <ngocbaopham1606@gmail.com>
2022-04-13 21:11:37 +0700

    finish

--- src/component/pages/learning/Learning.js
+++ src/component/pages/learning/Learning.js
@@ -1,86 +1,69 @@
 import "./Learning.css";
 
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
+function Learning() {
+  const courseList = useSelector((state) => state.courses.courseList);
+  //   console.log(courseList);
+  const courseListed = JSON.parse(localStorage.getItem("authUser"))[0]
+    .course_studied;
+  let listCourseStudied = [];
+  courseList.filter((item) => {
+    courseListed.filter((lesson) => {
+      if (item.idCoursesList === lesson) {
+        listCourseStudied.push(item);
+      }
+    });
+  });
+  console.log(listCourseStudied);
 
-
-function Learning(props) {
-    return (
+  return (
+    <>
+      {listCourseStudied.length !== 0 ? (
         <div className="container learning">
-            <div className="row">
-                <div className="col-12 col-md-6 col-xxl-4 mt-2">
-                    <div className="card ln-card">
-                        <Link to="#" className="ln-link">
-                            <div className="background">
-                            <button className="ln-btn">Học tiếp</button>
-                            </div>
-                        </Link>
-                        
-                        <div className="card-body">
-                            <h5 className="card-title">Kiến Thức Nhập Môn IT</h5>
-                            <p className="card-text ln-views">180.000 lượt xem</p>
-                        </div>
+          <div className="row">
+            {listCourseStudied.map((item, index) => {
+              return (
+                <div className="col-12 col-md-6 col-xxl-4 mt-2" key={index}>
+                  <div className="card ln-card">
+                    <Link to="#" className="ln-link">
+                      <div className="background">
+                        <button className="ln-btn">Học tiếp</button>
+                      </div>
+                    </Link>
+                    <div className="card-body">
+                      <h5 className="card-title">{item.name}</h5>
+                      <p className="card-text ln-views">180.000 lượt xem</p>
                     </div>
+                  </div>
                 </div>
-                <div className="col-12 col-md-6 col-xxl-4 pe-2">
-                    <div className="card ln-card">
-                        <Link to="#" className="ln-link">
-                            <div className="background">
-                            <button className="ln-btn">Học tiếp</button>
-                            </div>
-                        </Link>
-                        
-                        <div className="card-body">
-                            <h5 className="card-title">Kiến Thức Nhập Môn IT</h5>
-                            <p className="card-text ln-views">180.000 lượt xem</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12 col-md-6 mt-md-3 col-xxl-4 pe-2">
-                    <div className="card ln-card">
-                        <Link to="#" className="ln-link">
-                            <div className="background">
-                            <button className="ln-btn">Học tiếp</button>
-                            </div>
-                        </Link>
-                        
-                        <div className="card-body">
-                            <h5 className="card-title">Kiến Thức Nhập Môn IT</h5>
-                            <p className="card-text ln-views">180.000 lượt xem</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12 col-md-6 mt-md-3 col-xxl-4 mt-xxl-3 pe-2">
-                    <div className="card ln-card">
-                        <Link to="#" className="ln-link">
-                            <div className="background">
-                            <button className="ln-btn">Học tiếp</button>
-                            </div>
-                        </Link>
-                        
-                        <div className="card-body">
-                            <h5 className="card-title">Kiến Thức Nhập Môn IT</h5>
-                            <p className="card-text ln-views">180.000 lượt xem</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12 col-md-6 mt-md-3 col-xxl-4 mt-xxl-3 pe-2">
-                    <div className="card ln-card">
-                        <Link to="#" className="ln-link">
-                            <div className="background">
-                            <button className="ln-btn">Học tiếp</button>
-                            </div>
-                        </Link>
-                        
-                        <div className="card-body">
-                            <h5 className="card-title">Kiến Thức Nhập Môn IT</h5>
-                            <p className="card-text ln-views">180.000 lượt xem</p>
-                        </div>
-                    </div>
-                </div> 
-            </div>
+              );
+            })}
+          </div>
         </div>
-    )
-};
+      ) : (
+        <div className="learning_body">
+          <div className="learning_body_text">
+            <h2>Opps, KHÔNG CÓ KHÓA HỌC NÀO</h2>
+            <h3>Bạn chưa tham gia khóa học nào?</h3>
+            <Link to="/muc-luc">
+              <button className="btn-app-y button_jelly learning_body_button">
+                Đi đến khóa học
+              </button>
+            </Link>
+            <br />
+            <p style={{ fontSize: "1.1rem" }}>
+              Hoặc quay lại{" "}
+              <Link to="/" style={{ textDecoration: "none" }}>
+                Trang Chủ
+              </Link>
+            </p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
 
-export default Learning
+export default Learning;
