d9a54b351350186464331882045c5c8dccf21a58
dalnoon520 <dinhchi.a8.2001@gmail.com>
2022-04-13 10:39:46 +0700

    slideshow

--- src/component/pages/Home/Home.js
+++ src/component/pages/Home/Home.js
@@ -1,7 +1,27 @@
 import "./homepage.css";
 import { Link } from "react-router-dom";
+import slide1 from "../../../images/Slide1.png"
+import slide2 from "../../../images/Slide2.png"
+import slide3 from "../../../images/Slide3.png"
+
+
 
 function Home(props) {
+      var myIndex = 0;
+      carousel();
+
+    function carousel() {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+        console.log(x[i].style.display); 
+      }
+      myIndex++;
+      if (myIndex > x.length) {myIndex = 1}    
+      x[myIndex-1] ? x[myIndex-1].style.display = "block" : "";  
+      setTimeout(carousel, 2000); // Change image every 2 seconds
+    }
   return (
     <>
       <div className="container homepage-container">
@@ -89,11 +109,86 @@ function Home(props) {
         </div>
       </div>
       <div className="homepage_layout_3">
-        <div>
-          <h1>120.000+</h1>
-          <h2>nguời khác đã học</h2>
+        <div className="w3-content w3-section">
+          <div className="mySlides">
+            <div className="row slide-child">
+              <div className="col-4">
+                <div className="slide-content">
+                  <img className="slide-img" src={slide1} />
+                  <div className="slide-if">
+                    Mr Do Khanh Tu <br></br>
+                    Software Engineer at Amazon
+                  </div>
+                </div>
+              </div>
+              <div className="col-8 abc">
+                <p className="slide-text">"It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on Cocoders. Within a year I had a six-figure job as a Software Engineer. CoCoders changed my life." </p>
+              </div>
+            </div>
+          </div>  
+          <div className="mySlides">
+            <div className="row slide-child">
+            <div className="col-8 abc">
+                <p className="slide-text">I've always struggled with learning JavaScript. I've taken many courses but Cocoders's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on Cocoders gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify. </p>
+              </div>
+              <div className="col-4">
+                <div className="slide-content">
+                  <img className="slide-img" src={slide2} />
+                  <div className="slide-if">
+                    Ms Wendy <br></br>
+                    Software Engineer at Spotify
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>  
+
+          <div className="mySlides">
+            <div className="row slide-child">
+              <div className="col-4">
+                <div className="slide-content">
+                  <img className="slide-img" src={slide3} />
+                  <div className="slide-if">
+                    Ms Tu Anh <br></br>
+                    Software Engineer at Microsoft
+                  </div>
+                </div>
+              </div>
+              <div className="col-8 abc">
+                <p className="slide-text">Cocoders was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company." </p>
+              </div>
+            </div>
+          </div> 
+
+          
+
+          {/* <div className="mySlides">
+            <div className="row slide-child">
+              <p className="col-8 slide-text">"I've always struggled with learning JavaScript. I've taken many courses but Cocoders's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on Cocoders gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify." </p>
+              <div className="col-4 slide-content">
+              <img className="slide-img" src={slide2} />
+
+              </div>
+            </div>
+          </div>
+
+          <div className="mySlides">
+            <div className="row slide-child">
+              <div className="col-4 slide-content">
+              <img className="slide-img" src={slide3} />
+
+              </div>
+              <p className="col-8 slide-text">"Cocoders was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."</p>
+            </div>
+          </div> */}
+
         </div>
       </div>
+        {/* <div>
+          <h1>120.000+</h1>
+          <h2>nguời khác đã học</h2>
+        </div> */}
       {/* Layout 6 */}
       {/* <div className="container">
         <div className="homepage_layout_5">
